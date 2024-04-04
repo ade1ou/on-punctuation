dc9b451fdcf1b1c5d1e7e87cdc38253bd92312b4
ade1ou <plara312@newschool.edu>
2024-04-04 16:07:15 -0400

    thu

--- P2/Products/products.js
+++ P2/Products/products.js
@@ -17,7 +17,7 @@ const tasks = [
     {
       "title": "Blue Car Hawaiian Shirt",
       "img": 'Hawaiian shirts/blue-car-surf-parm-shirt.png',
-      "category": 'blue,car,surf,palm trees',
+      "category": 'blue, cars, surfers, palmtrees',
   },
   {
     "title": "Blue flower Hawaiian Shirt",
@@ -82,11 +82,66 @@ const tasks = [
   {
     "title": "Blue hula waves Hawaiian Shirt",
     "img": 'Hawaiian shirts/blue-hula-wave-flower-surf-shirt.png',
-    "category": 'blue, flowers, hula, waves, surf',
+    "category": 'blue, flowers, huladancers, beaches, surfers',
+  },
+  {
+    "title": "Blue pineapples palmtrees flower Hawaiian Shirt",
+    "img": 'Hawaiian shirts/blue-pineapple-palm-flower-surf-fish-shirt.png',
+    "category": 'blue, pineapples, palmtrees, beaches, flowers,fish,surfers',
+  },
+  {
+    "title": "Blue pineapples beaches Hawaiian Shirt",
+    "img": 'Hawaiian shirts/blue-pineapple-palm-shirt.png',
+    "category": 'blue, pineapples, palmtrees',
+  },
+  {
+    "title": "Blue pineapples Hawaiian Shirt",
+    "img": 'Hawaiian shirts/blue-pineapple-shirt.png',
+    "category": 'blue, pineapples',
+  },
+  {
+    "title": "Blue pineapples ukulele Hawaiian Shirt",
+    "img": 'Hawaiian shirts/blue-pineapple-ukulele-shirt.png',
+    "category": 'blue, pineapples,ukuleles',
+  },
+  {
+    "title": "Blue wave Hawaiian Shirt",
+    "img": 'Hawaiian shirts/blue-wave-shirt.png',
+    "category": 'blue, beaches',
+  },
+  {
+    "title": "Blue white pineapple flower Hawaiian Shirt",
+    "img": 'Hawaiian shirts/blue-white=pineapple-flower-shirt.png',
+    "category": 'blue, white, pineapples, flowers',
+  },
+  {
+    "title": "fish parmtree beaches Hawaiian Shirt",
+    "img": 'Hawaiian shirts/fish-palm-waves-blue-shirt.png',
+    "category": 'fish, palmtrees, beaches, blue',
+  },
+  {
+    "title": "flowers blue Hawaiian Shirt",
+    "img": 'Hawaiian shirts/flower-blue-shirt.png',
+    "category": 'flowers, blue',
+  },
+  {
+    "title": "flowers blue Hawaiian Shirt",
+    "img": 'Hawaiian shirts/flower-pattern-blue-shirt.png',
+    "category": 'flowers, blue',
+  },
+  {
+    "title": "flowers white green pink Hawaiian Shirt",
+    "img": 'Hawaiian shirts/flower-white-green-pink-shirt.png',
+    "category": 'flowers, white, green, pink',
+  },
+  {
+    "title": "green yellow fish Hawaiian Shirt",
+    "img": 'Hawaiian shirts/green-yellow-fish-shirt.png',
+    "category": 'yellow, fish, green',
   },
     // Add more tasks as needed
   ];
-    //  Hawaiian shirts/blue-pineapple-palm-flower-surf-fish-shirt.png Hawaiian shirts/blue-pineapple-palm-pattern.png Hawaiian shirts/blue-pineapple-palm-shirt.png Hawaiian shirts/blue-pineapple-pattern.png Hawaiian shirts/blue-pineapple-shirt.png Hawaiian shirts/blue-pineapple-ukulele-pattern.png Hawaiian shirts/blue-pineapple-ukulele-shirt.png Hawaiian shirts/blue-wave-pattern.png Hawaiian shirts/blue-wave-shirt.png Hawaiian shirts/blue-white=pineapple-flower-pattern.png Hawaiian shirts/blue-white=pineapple-flower-shirt.png Hawaiian shirts/fish-palm-waves-blue-pattern.png Hawaiian shirts/fish-palm-waves-blue-shirt.png Hawaiian shirts/flower-blue-pattern.jpg Hawaiian shirts/flower-blue-shirt.png Hawaiian shirts/flower-pattern-blue-pattern.jpg Hawaiian shirts/flower-pattern-blue-shirt.png Hawaiian shirts/flower-white-green-pink-pattern.jpg Hawaiian shirts/flower-white-green-pink-shirt.png Hawaiian shirts/green-yellow-fish-pattern.png Hawaiian shirts/green-yellow-fish-shirt.png Hawaiian shirts/hula-blue-ukulele-pineapple-flower-pattern.png Hawaiian shirts/hula-blue-ukulele-pineapple-flower-shirt.png Hawaiian shirts/hula-palm-flower-blue-pattern.png Hawaiian shirts/hula-palm-flower-blue-shirt.png Hawaiian shirts/island-palm-blue-pattern.png Hawaiian shirts/island-palm-blue-shirt.png Hawaiian shirts/orange-flower-palm-wave-pattern.png Hawaiian shirts/orange-flower-palm-wave-shirt.png Hawaiian shirts/palm-white-pattern.png Hawaiian shirts/palm-white-shirt.png Hawaiian shirts/pink-flower-pattern.png Hawaiian shirts/pink-flower-shirt.png Hawaiian shirts/pink-purple-flower-palm-pattern.jpg Hawaiian shirts/pink-purple-flower-palm-shirt.png Hawaiian shirts/pink-red-palm-hula-flower-pattern.png Hawaiian shirts/pink-red-palm-hula-flower-shirt.png Hawaiian shirts/pink-red-pineapple-palm-hula-ukulele-pattern.jpg Hawaiian shirts/pink-red-pineapple-palm-hula-ukulele-shirt.png Hawaiian shirts/purple-flower-pattern.png Hawaiian shirts/purple-flower-pattern2.png Hawaiian shirts/purple-flower-shirt.png Hawaiian shirts/purple-flower-shirt2.png Hawaiian shirts/red-flower-pattern.png Hawaiian shirts/red-flower-shirt.png Hawaiian shirts/red-hula-wave-flower-palm-pattern.png Hawaiian shirts/red-hula-wave-flower-palm-shirt.png Hawaiian shirts/red-pineapple-flower-pattern.png Hawaiian shirts/red-pineapple-flower-shirt.png Hawaiian shirts/red-white-flower-pattern.png Hawaiian shirts/red-white-flower-shirt.png Hawaiian shirts/white-flower-pattern.png Hawaiian shirts/white-flower-shirt.png Hawaiian shirts/white-yellow-green-flower-pattern.png Hawaiian shirts/white-yellow-green-flower-shirt.png
+// green-yellow-fish-shirt.png Hawaiian shirts/hula-blue-ukulele-pineapple-flower-pattern.png Hawaiian shirts/hula-blue-ukulele-pineapple-flower-shirt.png Hawaiian shirts/hula-palm-flower-blue-pattern.png Hawaiian shirts/hula-palm-flower-blue-shirt.png Hawaiian shirts/island-palm-blue-pattern.png Hawaiian shirts/island-palm-blue-shirt.png Hawaiian shirts/orange-flower-palm-wave-pattern.png Hawaiian shirts/orange-flower-palm-wave-shirt.png Hawaiian shirts/palm-white-pattern.png Hawaiian shirts/palm-white-shirt.png Hawaiian shirts/pink-flower-pattern.png Hawaiian shirts/pink-flower-shirt.png Hawaiian shirts/pink-purple-flower-palm-pattern.jpg Hawaiian shirts/pink-purple-flower-palm-shirt.png Hawaiian shirts/pink-red-palm-hula-flower-pattern.png Hawaiian shirts/pink-red-palm-hula-flower-shirt.png Hawaiian shirts/pink-red-pineapple-palm-hula-ukulele-pattern.jpg Hawaiian shirts/pink-red-pineapple-palm-hula-ukulele-shirt.png Hawaiian shirts/purple-flower-pattern.png Hawaiian shirts/purple-flower-pattern2.png Hawaiian shirts/purple-flower-shirt.png Hawaiian shirts/purple-flower-shirt2.png Hawaiian shirts/red-flower-pattern.png Hawaiian shirts/red-flower-shirt.png Hawaiian shirts/red-hula-wave-flower-palm-pattern.png Hawaiian shirts/red-hula-wave-flower-palm-shirt.png Hawaiian shirts/red-pineapple-flower-pattern.png Hawaiian shirts/red-pineapple-flower-shirt.png Hawaiian shirts/red-white-flower-pattern.png Hawaiian shirts/red-white-flower-shirt.png Hawaiian shirts/white-flower-pattern.png Hawaiian shirts/white-flower-shirt.png Hawaiian shirts/white-yellow-green-flower-pattern.png Hawaiian shirts/white-yellow-green-flower-shirt.png
   
     // Function to render images based on selected categories
 function renderContent() {
