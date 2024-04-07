2cfdb4e5864909225d50e895ec1e30efe96366ed
ade1ou <plara312@newschool.edu>
2024-04-06 23:17:46 -0400

    again

--- P2/Products/products.js
+++ P2/Products/products.js
@@ -2,182 +2,322 @@ const tasks = [
     {
         "title": "Blue Floral Hawaiian Shirt",
         "img": "Hawaiian shirts/anotherblueflower-shirt.png",
-        "category": 'flowers, blue',
+        "hoverImg": "Hawaiian shirts/anotherblueflower-pattern.png",
+        "category": 'Flowers, Blue',
     },
     {
         "title": "Black Hawaiian Shirt",
         "img": 'Hawaiian shirts/black-flower-shirt.png',
-        "category": 'black, flowers',
+        "hoverImg": "Hawaiian shirts/black-flower-pattern.png",
+        "category": 'Black, Flowers',
     },
     {
         "title": "Minimalst Floral Hawaiian Shirt",
         "img": 'Hawaiian shirts/black-green-red-flower-shirt.png',
-        "category": 'black, green, red, flowers',
+        "hoverImg": "Hawaiian shirts/black-green-red-flower-pattern.png",
+        "category": 'Black, Green, Red, Flowers',
     },
     {
       "title": "Blue Car Hawaiian Shirt",
       "img": 'Hawaiian shirts/blue-car-surf-parm-shirt.png',
-      "category": 'blue, cars, surfers, palmtrees',
+      "hoverImg": "Hawaiian shirts/blue-car-surf-parm-pattern.png",
+      "category": 'Blue, Cars, Surfers, Palm Trees',
   },
   {
     "title": "Blue flower Hawaiian Shirt",
     "img": 'Hawaiian shirts/blue-flower-shirt.png',
-    "category": 'blue, flowers',
+    "hoverImg": "Hawaiian shirts/blue-flower-pattern.png",
+    "category": 'Blue, Flowers',
   },
   {
     "title": "Blue flower Hawaiian Shirt",
     "img": 'Hawaiian shirts/blue-flower-shirt2.png',
-    "category": 'blue, flowers',
+    "hoverImg": "Hawaiian shirts/blue-flower-pattern2.png",
+    "category": 'Blue, Flowers',
   },
   {
     "title": "Blue flower Hawaiian Shirt",
     "img": 'Hawaiian shirts/blue-flower-shirt3.png',
-    "category": 'blue, flowers',
+    "hoverImg": "Hawaiian shirts/blue-flower-pattern3.png",
+    "category": 'Blue, Flowers',
   },
   {
     "title": "Blue flower Hawaiian Shirt",
     "img": 'Hawaiian shirts/blue-flower-shirt4.png',
-    "category": 'blue, flowers',
+    "hoverImg": "Hawaiian shirts/blue-flower-pattern4.png",
+    "category": 'Blue, Flowers',
   },
   {
     "title": "Blue flower Hawaiian Shirt",
     "img": 'Hawaiian shirts/blue-flower-shirt6.png',
-    "category": 'blue, flowers',
+    "hoverImg": "Hawaiian shirts/blue-flower-pattern6.png",
+    "category": 'Blue, Flowers',
   },
   {
     "title": "Blue flower Hawaiian Shirt",
     "img": 'Hawaiian shirts/blue-flower-shirt7.png',
-    "category": 'blue, flowers',
+    "hoverImg": "Hawaiian shirts/blue-flower-pattern7.png",
+    "category": 'Blue, Flowers',
   },
   {
     "title": "Blue flower Hawaiian Shirt",
     "img": 'Hawaiian shirts/blue-flower-shirt8.png',
-    "category": 'blue, flowers',
+    "hoverImg": "Hawaiian shirts/blue-flower-pattern8.png",
+    "category": 'Blue, Flowers',
   },
   {
     "title": "Blue flower Hawaiian Shirt",
     "img": 'Hawaiian shirts/blue-flower-shirt9.png',
-    "category": 'blue, flowers',
+    "hoverImg": "Hawaiian shirts/blue-flower-pattern9.png",
+    "category": 'Blue, Flowers',
   },
   {
     "title": "Blue flower Hawaiian Shirt",
     "img": 'Hawaiian shirts/blue-flower-shirt10.png',
-    "category": 'blue, flowers',
+    "hoverImg": "Hawaiian shirts/blue-flower-pattern10.png",
+    "category": 'Blue, Flowers',
   },
   {
     "title": "Blue flower Hawaiian Shirt",
     "img": 'Hawaiian shirts/blue-flower-shirt11.png',
-    "category": 'blue, flowers',
+    "hoverImg": "Hawaiian shirts/blue-flower-pattern11.png",
+    "category": 'Blue, Flowers',
   },
   {
     "title": "Blue flower waves Hawaiian Shirt",
     "img": 'Hawaiian shirts/blue-flower-wave-shirt.png',
-    "category": 'blue, flowers, waves',
+    "hoverImg": "Hawaiian shirts/blue-flower-wave-pattern.png",
+    "category": 'Blue, Flowers, Beaches',
   },
   {
     "title": "Blue hula and fish Hawaiian Shirt",
     "img": 'Hawaiian shirts/blue-hula-fish-wave-palm-flower-shirt.png',
-    "category": 'blue, flowers, palm trees, fish, hula, waves',
+    "hoverImg": "Hawaiian shirts/blue-hula-fish-wave-palm-flower-pattern.png",
+    "category": 'Blue, Flowers, Palm Trees, Fish, Hula Dancers, Beaches',
   },
   {
     "title": "Blue hula waves Hawaiian Shirt",
     "img": 'Hawaiian shirts/blue-hula-wave-flower-surf-shirt.png',
-    "category": 'blue, flowers, huladancers, beaches, surfers',
+    "hoverImg": "Hawaiian shirts/blue-hula-wave-flower-surf-pattern.png",
+    "category": 'Blue, Flowers, Hula Dancers, Beaches, Surfers',
   },
   {
     "title": "Blue pineapples palmtrees flower Hawaiian Shirt",
     "img": 'Hawaiian shirts/blue-pineapple-palm-flower-surf-fish-shirt.png',
-    "category": 'blue, pineapples, palmtrees, beaches, flowers,fish,surfers',
+    "hoverImg": "Hawaiian shirts/blue-pineapple-palm-flower-surf-fish-pattern.png",
+    "category": 'Blue, Pineapples, Palm Trees, Beaches, Flowers, Fish, Surfers',
   },
   {
     "title": "Blue pineapples beaches Hawaiian Shirt",
     "img": 'Hawaiian shirts/blue-pineapple-palm-shirt.png',
-    "category": 'blue, pineapples, palmtrees',
+    "hoverImg": "Hawaiian shirts/blue-pineapple-palm-pattern.png",
+    "category": 'Blue, Pineapples, Palm Trees',
   },
   {
     "title": "Blue pineapples Hawaiian Shirt",
     "img": 'Hawaiian shirts/blue-pineapple-shirt.png',
-    "category": 'blue, pineapples',
+    "hoverImg": "Hawaiian shirts/blue-pineapple-pattern.png",
+    "category": 'Blue, Pineapples',
   },
   {
     "title": "Blue pineapples ukulele Hawaiian Shirt",
     "img": 'Hawaiian shirts/blue-pineapple-ukulele-shirt.png',
-    "category": 'blue, pineapples,ukuleles',
+    "hoverImg": "Hawaiian shirts/blue-pineapple-ukulele-pattern.png",
+    "category": 'Blue, Pineapples, Ukuleles',
   },
   {
     "title": "Blue wave Hawaiian Shirt",
     "img": 'Hawaiian shirts/blue-wave-shirt.png',
-    "category": 'blue, beaches',
+    "hoverImg": "Hawaiian shirts/blue-wave-pattern.png",
+    "category": 'Blue, Beaches',
   },
   {
     "title": "Blue white pineapple flower Hawaiian Shirt",
     "img": 'Hawaiian shirts/blue-white=pineapple-flower-shirt.png',
-    "category": 'blue, white, pineapples, flowers',
+    "hoverImg": "Hawaiian shirts/blue-white=pineapple-flower-pattern.png",
+    "category": 'Blue, White, Pineapples, Flowers',
   },
   {
     "title": "fish parmtree beaches Hawaiian Shirt",
     "img": 'Hawaiian shirts/fish-palm-waves-blue-shirt.png',
-    "category": 'fish, palmtrees, beaches, blue',
+    "hoverImg": "Hawaiian shirts/fish-palm-waves-blue-pattern.png",
+    "category": 'Fish, Palm Trees, Beaches, Blue',
   },
   {
     "title": "flowers blue Hawaiian Shirt",
     "img": 'Hawaiian shirts/flower-blue-shirt.png',
-    "category": 'flowers, blue',
+    "hoverImg": "Hawaiian shirts/flower-blue-pattern.png",
+    "category": 'Flowers, Blue',
   },
   {
     "title": "flowers blue Hawaiian Shirt",
     "img": 'Hawaiian shirts/flower-pattern-blue-shirt.png',
-    "category": 'flowers, blue',
+    "hoverImg": "Hawaiian shirts/flower-pattern-blue-pattern.png",
+    "category": 'Flowers, Blue',
   },
   {
     "title": "flowers white green pink Hawaiian Shirt",
     "img": 'Hawaiian shirts/flower-white-green-pink-shirt.png',
-    "category": 'flowers, white, green, pink',
+    "hoverImg": "Hawaiian shirts/flower-white-green-pink-pattern.png",
+    "category": 'Flowers, White, Green, Pink',
   },
   {
     "title": "green yellow fish Hawaiian Shirt",
     "img": 'Hawaiian shirts/green-yellow-fish-shirt.png',
-    "category": 'yellow, fish, green',
+    "hoverImg": "Hawaiian shirts/green-yellow-fish-pattern.png",
+    "category": 'Yellow, Fish, Green',
   },
-    // Add more tasks as needed
+  {
+    "title": "Blue Hula Ukulele pineapple flower Hawaiian Shirt",
+    "img": 'Hawaiian shirts/hula-blue-ukulele-pineapple-flower-shirt.png',
+    "hoverImg": "Hawaiian shirts/hula-blue-ukulele-pineapple-flower-pattern.png",
+    "category": 'Hula Dancers, Flowers, Blue, Ukuleles, Pineapples',
+  },
+  {
+    "title": "Blue Hula Palm Flower Hawaiian Shirt",
+    "img": 'Hawaiian shirts/hula-palm-flower-blue-shirt.png',
+    "hoverImg": "Hawaiian shirts/hula-palm-flower-blue-pattern.png",
+    "category": 'Hula Dancers, Palm Trees, Blue, Flowers',
+  },
+  {
+    "title": "Island Palm Blue Hawaiian Shirt",
+    "img": 'Hawaiian shirts/island-palm-blue-shirt.png',
+    "hoverImg": "Hawaiian shirts/island-palm-blue-pattern.png",
+    "category": 'Beaches, Palm Trees, Blue',
+  },
+  {
+    "title": "Orange Flowers Palm wave Hawaiian Shirt",
+    "img": 'Hawaiian shirts/orange-flower-palm-wave-shirt.png',
+    "hoverImg": "Hawaiian shirts/orange-flower-palm-wave-pattern.png",
+    "category": 'Beaches, Palm Trees, Orange, Flowers',
+  },
+  {
+    "title": "Palm white Hawaiian Shirt",
+    "img": 'Hawaiian shirts/palm-white-shirt.png',
+    "hoverImg": "Hawaiian shirts/palm-white-pattern.png",
+    "category": 'White, Palm Trees',
+  },
+  {
+    "title": "Pink flowers Hawaiian Shirt",
+    "img": 'Hawaiian shirts/pink-flower-shirt.png',
+    "hoverImg": "Hawaiian shirts/pink-flower-pattern.png",
+    "category": 'Pink, Flowers',
+  },
+  {
+    "title": "Pink Purple flowers palm Hawaiian Shirt",
+    "img": 'Hawaiian shirts/pink-purple-flower-palm-shirt.png',
+    "hoverImg": "Hawaiian shirts/pink-purple-flower-palm-pattern.png",
+    "category": 'Pink, Flowers, Purple, Palm Trees',
+  },
+  {
+    "title": "Pink red flowers palm Hawaiian Shirt",
+    "img": 'Hawaiian shirts/pink-red-palm-hula-flower-shirt.png',
+    "hoverImg": "Hawaiian shirts/pink-red-palm-hula-flower-pattern.png",
+    "category": 'Pink, Flowers, Red, Palm Trees, Hula Dancers',
+  },
+  {
+    "title": "Pink red pineapple palm hula ukuleleHawaiian Shirt",
+    "img": 'Hawaiian shirts/pink-red-pineapple-palm-hula-ukulele-shirt.png',
+    "hoverImg": "Hawaiian shirts/pink-red-pineapple-palm-hula-ukulele-pattern.png",
+    "category": 'Pink, Pineapples, Red, Palm Trees, Hula Dancers, Ukuleles',
+  },
+  {
+    "title": "Purple flower Hawaiian Shirt",
+    "img": 'Hawaiian shirts/purple-flower-shirt2.png',
+    "hoverImg": "Hawaiian shirts/purple-flower-pattern2.png",
+    "category": 'Purple, Flowers',
+  },
+  {
+    "title": "Purple flower Hawaiian Shirt",
+    "img": 'Hawaiian shirts/purple-flower-shirt.png',
+    "hoverImg": "Hawaiian shirts/purple-flower-pattern.png",
+    "category": 'Purple, Flowers',
+  },
+  {
+    "title": "Red flower Hawaiian Shirt",
+    "img": 'Hawaiian shirts/red-flower-shirt.png',
+    "hoverImg": "Hawaiian shirts/red-flower-pattern.png",
+    "category": 'Red, Flowers',
+  },
+  {
+    "title": "Red hula palm flower Hawaiian Shirt",
+    "img": 'Hawaiian shirts/red-hula-wave-flower-palm-shirt.png',
+    "hoverImg": "Hawaiian shirts/red-hula-wave-flower-palm-pattern.png",
+    "category": 'Red, Flowers, Hula Dancers, Beaches, Palm Trees',
+  },
+  {
+    "title": "Red Pineapple  flower Hawaiian Shirt",
+    "img": 'Hawaiian shirts/red-pineapple-flower-shirt.png',
+    "hoverImg": "Hawaiian shirts/red-pineapple-flower-pattern.png",
+    "category": 'Red, Flowers, Pineapples',
+  },
+  {
+    "title": "Red White flower Hawaiian Shirt",
+    "img": 'Hawaiian shirts/red-white-flower-shirt.png',
+    "hoverImg": "Hawaiian shirts/red-white-flower-pattern.png",
+    "category": 'Red, Flowers, White',
+  },
+  {
+    "title": "White flower Hawaiian Shirt",
+    "img": 'Hawaiian shirts/white-flower-shirt.png',
+    "hoverImg": "Hawaiian shirts/white-flower-pattern.png",
+    "category": 'Flowers, White',
+  },
+  {
+    "title": "White yellow green flower Hawaiian Shirt",
+    "img": 'Hawaiian shirts/white-yellow-green-flower-shirt.png',
+    "hoverImg": "Hawaiian shirts/white-yellow-green-flower-pattern.png",
+    "category": 'Flowers, White, Yellow, Green',
+  }
   ];
-// green-yellow-fish-shirt.png Hawaiian shirts/hula-blue-ukulele-pineapple-flower-pattern.png Hawaiian shirts/hula-blue-ukulele-pineapple-flower-shirt.png Hawaiian shirts/hula-palm-flower-blue-pattern.png Hawaiian shirts/hula-palm-flower-blue-shirt.png Hawaiian shirts/island-palm-blue-pattern.png Hawaiian shirts/island-palm-blue-shirt.png Hawaiian shirts/orange-flower-palm-wave-pattern.png Hawaiian shirts/orange-flower-palm-wave-shirt.png Hawaiian shirts/palm-white-pattern.png Hawaiian shirts/palm-white-shirt.png Hawaiian shirts/pink-flower-pattern.png Hawaiian shirts/pink-flower-shirt.png Hawaiian shirts/pink-purple-flower-palm-pattern.jpg Hawaiian shirts/pink-purple-flower-palm-shirt.png Hawaiian shirts/pink-red-palm-hula-flower-pattern.png Hawaiian shirts/pink-red-palm-hula-flower-shirt.png Hawaiian shirts/pink-red-pineapple-palm-hula-ukulele-pattern.jpg Hawaiian shirts/pink-red-pineapple-palm-hula-ukulele-shirt.png Hawaiian shirts/purple-flower-pattern.png Hawaiian shirts/purple-flower-pattern2.png Hawaiian shirts/purple-flower-shirt.png Hawaiian shirts/purple-flower-shirt2.png Hawaiian shirts/red-flower-pattern.png Hawaiian shirts/red-flower-shirt.png Hawaiian shirts/red-hula-wave-flower-palm-pattern.png Hawaiian shirts/red-hula-wave-flower-palm-shirt.png Hawaiian shirts/red-pineapple-flower-pattern.png Hawaiian shirts/red-pineapple-flower-shirt.png Hawaiian shirts/red-white-flower-pattern.png Hawaiian shirts/red-white-flower-shirt.png Hawaiian shirts/white-flower-pattern.png Hawaiian shirts/white-flower-shirt.png Hawaiian shirts/white-yellow-green-flower-pattern.png Hawaiian shirts/white-yellow-green-flower-shirt.png
-  
-    // Function to render images based on selected categories
+
+
+
+
+// Function to create and append image elements to the toolbox
+function createImageElement(task) {
+    const taskElement = document.createElement('div');
+    taskElement.className = 'task';
+
+    const img = document.createElement('img');
+    img.src = task.img;
+    img.alt = task.title;
+
+    // Apply hover effect
+    img.addEventListener('mouseover', function() {
+        img.src = task.hoverImg;
+    });
+
+    img.addEventListener('mouseout', function() {
+        img.src = task.img;
+    });
+
+    taskElement.appendChild(img);
+    return taskElement;
+}
+
 function renderContent() {
     const toolbox = document.getElementById('toolbox');
     const filterOptions = document.querySelectorAll('#filter-options input[type="checkbox"]:checked');
     const selectedCategories = Array.from(filterOptions).map(option => option.value);
-  
+
     toolbox.innerHTML = '';
-  
+
     tasks.forEach(task => {
-      const categories = task.category.split(', ');
-  
-      // Check if any of the selected categories match the task's categories
-      if (selectedCategories.some(category => categories.includes(category))) {
-        const taskElement = document.createElement('div');
-        taskElement.className = 'task';
-  
-        const img = document.createElement('img');
-        img.src = task.img;
-        img.alt = task.title;
-  
-        const title = document.createElement('p');
-        title.textContent = task.title;
-  
-        taskElement.appendChild(img);
-        // taskElement.appendChild(title);
-        toolbox.appendChild(taskElement);
-      }
+        const categories = task.category.split(', ');
+
+        if (selectedCategories.some(category => categories.includes(category))) {
+            const taskElement = createImageElement(task);
+            toolbox.appendChild(taskElement);
+        }
     });
-  }
-  
-  // Event listener to re-render content when checkboxes are clicked
-  document.querySelectorAll('#filter-options input[type="checkbox"]').forEach(checkbox => {
+}
+
+
+// Event listener to re-render content when checkboxes are clicked
+document.querySelectorAll('#filter-options input[type="checkbox"]').forEach(checkbox => {
     checkbox.addEventListener('change', renderContent);
-  });
-  
-  // Initial render
-  renderContent();
-  
+});
+
+// Initial render
+renderContent();
