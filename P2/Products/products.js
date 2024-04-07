3d389fea91bb0015333992c5518cea293fd1ac24
ade1ou <plara312@newschool.edu>
2024-04-06 23:52:20 -0400

    again

--- P2/Products/products.js
+++ P2/Products/products.js
@@ -21,7 +21,7 @@ const tasks = [
       "title": "Blue Car Hawaiian Shirt",
       "img": 'Hawaiian shirts/blue-car-surf-parm-shirt.png',
       "hoverImg": "Hawaiian shirts/blue-car-surf-parm-pattern.png",
-      "category": 'Blue, Cars, Surfers, Palm Trees',
+      "category": 'Blue, Cars, Surfers, PalmTrees',
   },
   {
     "title": "Blue flower Hawaiian Shirt",
@@ -93,25 +93,25 @@ const tasks = [
     "title": "Blue hula and fish Hawaiian Shirt",
     "img": 'Hawaiian shirts/blue-hula-fish-wave-palm-flower-shirt.png',
     "hoverImg": "Hawaiian shirts/blue-hula-fish-wave-palm-flower-pattern.png",
-    "category": 'Blue, Flowers, Palm Trees, Fish, Hula Dancers, Beaches',
+    "category": 'Blue, Flowers, PalmTrees, Fish, HulaDancers, Beaches',
   },
   {
     "title": "Blue hula waves Hawaiian Shirt",
     "img": 'Hawaiian shirts/blue-hula-wave-flower-surf-shirt.png',
     "hoverImg": "Hawaiian shirts/blue-hula-wave-flower-surf-pattern.png",
-    "category": 'Blue, Flowers, Hula Dancers, Beaches, Surfers',
+    "category": 'Blue, Flowers, HulaDancers, Beaches, Surfers',
   },
   {
     "title": "Blue pineapples palmtrees flower Hawaiian Shirt",
     "img": 'Hawaiian shirts/blue-pineapple-palm-flower-surf-fish-shirt.png',
     "hoverImg": "Hawaiian shirts/blue-pineapple-palm-flower-surf-fish-pattern.png",
-    "category": 'Blue, Pineapples, Palm Trees, Beaches, Flowers, Fish, Surfers',
+    "category": 'Blue, Pineapples, PalmTrees, Beaches, Flowers, Fish, Surfers',
   },
   {
     "title": "Blue pineapples beaches Hawaiian Shirt",
     "img": 'Hawaiian shirts/blue-pineapple-palm-shirt.png',
     "hoverImg": "Hawaiian shirts/blue-pineapple-palm-pattern.png",
-    "category": 'Blue, Pineapples, Palm Trees',
+    "category": 'Blue, Pineapples, PalmTrees',
   },
   {
     "title": "Blue pineapples Hawaiian Shirt",
@@ -141,7 +141,7 @@ const tasks = [
     "title": "fish parmtree beaches Hawaiian Shirt",
     "img": 'Hawaiian shirts/fish-palm-waves-blue-shirt.png',
     "hoverImg": "Hawaiian shirts/fish-palm-waves-blue-pattern.png",
-    "category": 'Fish, Palm Trees, Beaches, Blue',
+    "category": 'Fish, PalmTrees, Beaches, Blue',
   },
   {
     "title": "flowers blue Hawaiian Shirt",
@@ -171,31 +171,31 @@ const tasks = [
     "title": "Blue Hula Ukulele pineapple flower Hawaiian Shirt",
     "img": 'Hawaiian shirts/hula-blue-ukulele-pineapple-flower-shirt.png',
     "hoverImg": "Hawaiian shirts/hula-blue-ukulele-pineapple-flower-pattern.png",
-    "category": 'Hula Dancers, Flowers, Blue, Ukuleles, Pineapples',
+    "category": 'HulaDancers, Flowers, Blue, Ukuleles, Pineapples',
   },
   {
     "title": "Blue Hula Palm Flower Hawaiian Shirt",
     "img": 'Hawaiian shirts/hula-palm-flower-blue-shirt.png',
     "hoverImg": "Hawaiian shirts/hula-palm-flower-blue-pattern.png",
-    "category": 'Hula Dancers, Palm Trees, Blue, Flowers',
+    "category": 'HulaDancers, PalmTrees, Blue, Flowers',
   },
   {
     "title": "Island Palm Blue Hawaiian Shirt",
     "img": 'Hawaiian shirts/island-palm-blue-shirt.png',
     "hoverImg": "Hawaiian shirts/island-palm-blue-pattern.png",
-    "category": 'Beaches, Palm Trees, Blue',
+    "category": 'Beaches, PalmTrees, Blue',
   },
   {
     "title": "Orange Flowers Palm wave Hawaiian Shirt",
     "img": 'Hawaiian shirts/orange-flower-palm-wave-shirt.png',
     "hoverImg": "Hawaiian shirts/orange-flower-palm-wave-pattern.png",
-    "category": 'Beaches, Palm Trees, Orange, Flowers',
+    "category": 'Beaches, PalmTrees, Orange, Flowers',
   },
   {
     "title": "Palm white Hawaiian Shirt",
     "img": 'Hawaiian shirts/palm-white-shirt.png',
     "hoverImg": "Hawaiian shirts/palm-white-pattern.png",
-    "category": 'White, Palm Trees',
+    "category": 'White, PalmTrees',
   },
   {
     "title": "Pink flowers Hawaiian Shirt",
@@ -207,19 +207,19 @@ const tasks = [
     "title": "Pink Purple flowers palm Hawaiian Shirt",
     "img": 'Hawaiian shirts/pink-purple-flower-palm-shirt.png',
     "hoverImg": "Hawaiian shirts/pink-purple-flower-palm-pattern.png",
-    "category": 'Pink, Flowers, Purple, Palm Trees',
+    "category": 'Pink, Flowers, Purple, PalmTrees',
   },
   {
     "title": "Pink red flowers palm Hawaiian Shirt",
     "img": 'Hawaiian shirts/pink-red-palm-hula-flower-shirt.png',
     "hoverImg": "Hawaiian shirts/pink-red-palm-hula-flower-pattern.png",
-    "category": 'Pink, Flowers, Red, Palm Trees, Hula Dancers',
+    "category": 'Pink, Flowers, Red, PalmTrees, HulaDancers',
   },
   {
     "title": "Pink red pineapple palm hula ukuleleHawaiian Shirt",
     "img": 'Hawaiian shirts/pink-red-pineapple-palm-hula-ukulele-shirt.png',
     "hoverImg": "Hawaiian shirts/pink-red-pineapple-palm-hula-ukulele-pattern.png",
-    "category": 'Pink, Pineapples, Red, Palm Trees, Hula Dancers, Ukuleles',
+    "category": 'Pink, Pineapples, Red, PalmTrees, HulaDancers, Ukuleles',
   },
   {
     "title": "Purple flower Hawaiian Shirt",
@@ -243,7 +243,7 @@ const tasks = [
     "title": "Red hula palm flower Hawaiian Shirt",
     "img": 'Hawaiian shirts/red-hula-wave-flower-palm-shirt.png',
     "hoverImg": "Hawaiian shirts/red-hula-wave-flower-palm-pattern.png",
-    "category": 'Red, Flowers, Hula Dancers, Beaches, Palm Trees',
+    "category": 'Red, Flowers, HulaDancers, Beaches, PalmTrees',
   },
   {
     "title": "Red Pineapple  flower Hawaiian Shirt",
@@ -295,6 +295,43 @@ function createImageElement(task) {
     taskElement.appendChild(img);
     return taskElement;
 }
+const soundEffects = {
+  Blue: 'ukulele sounds/sound1.mp3',
+  Green: 'ukulele sounds/sound2.mp3',
+  Pink: 'ukulele sounds/sound3.mp3',
+  Purple: 'ukulele sounds/sound4.mp3',
+  Yellow: 'ukulele sounds/sound5.mp3',
+  Orange: 'ukulele sounds/sound6.mp3',
+  Black: 'ukulele sounds/sound7.mp3',
+  White: 'ukulele sounds/sound8.mp3',
+  Red: 'ukulele sounds/sound9.mp3',
+  Flowers: 'ukulele sounds/sound10.mp3',
+  Surfers: 'ukulele sounds/sound11.mp3',
+  Beaches: 'ukulele sounds/sound12.mp3',
+  Ukuleles: 'ukulele sounds/sound13.mp3',
+  Cars: 'ukulele sounds/sound14.mp3',
+  Fish: 'ukulele sounds/sound15.mp3',
+  Pineapples: 'ukulele sounds/sound16.mp3',
+  HulaDancers:'ukulele sounds/sound17.mp3',
+  PalmTrees:'ukulele sounds/sound18.mp3',
+};
+
+function playSoundEffect(event) {
+  const checkbox = event.target;
+  const effect = checkbox.value;
+  const audioSrc = soundEffects[effect];
+  if (audioSrc) {
+    const audio = new Audio(audioSrc);
+    audio.play();
+  }
+}
+
+// Event listener to play sound effect when a checkbox is clicked
+document.querySelectorAll('#filter-options input[type="checkbox"]').forEach(checkbox => {
+    checkbox.addEventListener('change', playSoundEffect);
+});
+
+
 
 function renderContent() {
     const toolbox = document.getElementById('toolbox');
